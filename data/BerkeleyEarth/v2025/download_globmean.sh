71ed7b485727ed4edc559bcd79fe9a556a50920a
Mathias Hauser <mathias.hauser@env.ethz.ch>
2025-03-12 17:38:15 +0100

    BerkeleyEarth: only donwload if file does not exist

--- data/BerkeleyEarth/v2025/download_globmean.sh
+++ data/BerkeleyEarth/v2025/download_globmean.sh
@@ -4,6 +4,13 @@
 
 mkdir raw
 pushd raw
-wget https://berkeley-earth-temperature.s3.us-west-1.amazonaws.com/Global/Land_and_Ocean_summary.txt
+
+file=Land_and_Ocean_summary.txt
+
+if [ -f $file ]; then
+   echo "raw/$file already exists - delete it manually to download new version"
+else
+    wget https://berkeley-earth-temperature.s3.us-west-1.amazonaws.com/Global/Land_and_Ocean_summary.txt
+fi
 
 popd
